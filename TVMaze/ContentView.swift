7fd5d6af4d4bfacb809f138ef0f9fe1f33904eb6
Felipe Mendes <felipemendes@me.com>
2024-03-25 15:17:03 -0300

    Initial Commit

---
+++ TVMaze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TVMaze
+//
+//  Created by Felipe Mendes on 25/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TVMaze/TVMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeApp.swift
+//  TVMaze
+//
+//  Created by Felipe Mendes on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
