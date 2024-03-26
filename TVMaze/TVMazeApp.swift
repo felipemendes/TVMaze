40efec746f341c4e793c7514022c6f4a59ac93b8
Felipe Mendes <felipemendes@me.com>
2024-03-26 02:10:34 -0300

    Set up app initialization

--- TVMaze/TVMazeApp.swift
+++ TVMaze/TVMazeApp.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import TVMazeServiceKit
 
 @main
 struct TVMazeApp: App {
+
+    @StateObject private var showsViewModel: ShowsViewModel
+
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [
+            .foregroundColor: UIColor(Color.theme.accent)
+        ]
+
+        UINavigationBar.appearance().titleTextAttributes = [
+            .foregroundColor: UIColor(Color.theme.accent)
+        ]
+
+        let showDataService = ShowsRemoteDataService()
+        _showsViewModel = StateObject(wrappedValue: ShowsViewModel(showDataService: showDataService))
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ShowsView()
+            }
         }
+        .environmentObject(showsViewModel)
     }
 }
